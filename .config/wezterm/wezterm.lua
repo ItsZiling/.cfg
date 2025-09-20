cbdea8cac89174566d6963c0d306353bddeefe4d
Jason <jasonchen7766@gmail.com>
2025-09-20 14:31:06 -0400

    update tracked files

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -7,7 +7,7 @@ if wezterm.config_builder then config = wezterm.config_builder() end
 config.font_size = 18
 config.line_height = 1.2
 config.font = wezterm.font({
-  {family = "JetBrains Mono", scale = 1.2},
+  { family = "JetBrains Mono", scale = 1.2 },
 })
 
 -- Colors
@@ -19,9 +19,34 @@ config.color = {
 
 -- Windows and appearance
 config.window_decorations = "RESIZE"
-config.window_background_opacity = 0.9
 config.window_close_confirmation = "NeverPrompt"
 config.use_fancy_tab_bar = false
 config.default_cursor_style = "BlinkingBar"
+
+-- Background
+config = {
+  background = {
+    {
+      source = {
+        File = "$HOME/wallpapers/night.jpg"
+      },
+      hsb = { 
+        hue = 1.0,
+        saturation = 1.0,
+        brightness = 0.50,
+      },
+      width = "100%", 
+      height = "100%",
+    },
+    {
+      source = { 
+        Color = "#282c40",
+      },
+      width = "100%",
+      height = "100%",
+      opacity = 0.50,
+    },
+  },
+}
 
 return config
