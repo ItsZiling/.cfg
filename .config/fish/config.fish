12df4d034b00e8eddbd4a166e0a335cc1a65fd9b
Jason <jasonchen7766@gmail.com>
2025-09-20 14:32:04 -0400

    added fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+alias cfg='/usr/bin/git --git-dir=$HOME/.cfg --work-tree=$HOME'
+# starship init fish | source # decided to go with configuring fish instead
+
+set fish_greeting
