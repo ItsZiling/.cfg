e040fe2c16711120361b5998af869bdb6c741961
Jason <jasonchen7766@gmail.com>
2025-09-20 14:59:44 -0400

    update tracked files

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,4 +5,8 @@ end
 alias cfg='/usr/bin/git --git-dir=$HOME/.cfg --work-tree=$HOME'
 # starship init fish | source # decided to go with configuring fish instead
 
+if type -q opam 
+    eval (opam env --switch=5.2.0)
+end
+
 set fish_greeting
