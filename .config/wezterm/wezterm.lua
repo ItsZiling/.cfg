92887117e1777dec6bc2db1220b46f8e774fbc3f
zi <jasonchen7766@gmai.com>
2025-09-20 22:58:43 -0400

    updated tracked files

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,50 +3,46 @@ local config = {}
 
 if wezterm.config_builder then config = wezterm.config_builder() end
 
+
+config.automatically_reload_config = true
 -- font
-config.font_size = 18
+config.font_size = 10
 config.line_height = 1.2
-config.font = wezterm.font({
-  { family = "JetBrains Mono", scale = 1.2 },
+config.color_scheme = "Tokyo Night"
+config.font = wezterm.font_with_fallback({
+  { family = "JetBrains Mono", scale = 1 },
 })
 
 -- Colors
-config.color_scheme = "Tokyo-night"
-config.color = {
+config.colors = {
   cursor_bg = "white",
   cursor_border = "white"
 }
 
 -- Windows and appearance
 config.window_decorations = "RESIZE"
+config.window_background_opacity = 0.9
 config.window_close_confirmation = "NeverPrompt"
-config.use_fancy_tab_bar = false
 config.default_cursor_style = "BlinkingBar"
+config.hide_tab_bar_if_only_one_tab = true
 
--- Background
-config = {
-  background = {
-    {
-      source = {
-        File = "$HOME/wallpapers/purple_night.jpg"
-      },
-      hsb = { 
-        hue = 1.0,
-        saturation = 1.0,
-        brightness = 0.50,
-      },
-      width = "100%", 
-      height = "100%",
-    },
-    {
-      source = { 
-        Color = "#282c40",
-      },
-      width = "100%",
-      height = "100%",
-      opacity = 0.50,
-    },
+
+-- Key Bindings
+config.keys = {
+  { 
+    key = "q", 
+    mods ="CTRL", 
+    action = wezterm.action.CloseCurrentPane {confirm = true},
   },
 }
 
+
+-- Mouse Bindings
+config.mouse_bindings = {
+  {
+    event = { Drag = { streak = 1, button = 'Left' } },
+    mods = "CTRL",
+    action = wezterm.action.StartWindowDrag,
+  },
+}
 return config
