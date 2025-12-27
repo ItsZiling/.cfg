ff17aed712d8630c837315de634ef9944bfcf753
Jason Chen <jasonchen7766@gmail.com>
2025-12-27 13:04:23 -0500

    Refactor bar.lua to include color module and settings

--- .config/oxwm/settings/bar.lua
+++ .config/oxwm/settings/bar.lua
@@ -1,4 +1,5 @@
 -- Use fc-list to see all fonts
+local colors = require("colors.lua")
 local bar_font = "Jetbrains Mono:style=Bold:size=14"
 local blocks = {
     oxwm.bar.block.ram({
@@ -59,3 +60,4 @@ oxwm.bar.set_scheme_occupied(colors.cyan, colors.bg, colors.cyan)
 oxwm.bar.set_scheme_selected(colors.cyan, colors.bg, colors.purple)
 -- Urgent tags (windows requesting attention)
 oxwm.bar.set_scheme_urgent(colors.red, colors.bg, colors.red)
+
