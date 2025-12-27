7c6413daa1246af31170004c19093d2f36082e65
Jason Chen <jasonchen7766@gmail.com>
2025-12-27 12:18:56 -0500

    Add default decorations configuration for oxwm

---
+++ .config/oxwm/settings/decorations.lua
@@ -0,0 +1,19 @@
+-- These are default config for oxwm
+
+local colors = require("colors.lua")
+
+-- Width in pixels
+oxwm.border.set_width(2)
+-- Color of focused window border
+oxwm.border.set_focused_color(colors.blue)
+-- Color of unfocused window borders
+oxwm.border.set_unfocused_color(colors.grey)
+
+-- Smart Enabled = No border if 1 window
+oxwm.gaps.set_smart(enabled)
+-- Inner gaps (horizontal, vertical) in pixels
+oxwm.gaps.set_inner(5, 5)
+-- Outer gaps (horizontal, vertical) in pixels
+oxwm.gaps.set_outer(5, 5)
+
+
