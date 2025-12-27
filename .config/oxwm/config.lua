11e7c15a794aaeba1d7a4ac42e9db9693e7a7949
Jason Chen <jasonchen7766@gmail.com>
2025-12-27 13:10:07 -0500

    Add basic settings for tags and layout
    Added basic settings for tags and layout symbols.

--- .config/oxwm/config.lua
+++ .config/oxwm/config.lua
@@ -1,7 +1,15 @@
 --- @module 'oxwm'
 
-require("settings.binds")
+-- basic settings
+local tags = { "", "󰊯", "", "", "󰙯", "󱇤", "", "󱘶", "󰧮" } 
+oxwm.set_tags(tags)
+oxwm.set_layout_symbol("tiling", "[T]")
+oxwm.set_layout_symbol("normie", "[F]")
 
+require("settings.binds")
+require("settings.bar")
+require("settings.decorations")
+require("settings.rules")
 
 
 
